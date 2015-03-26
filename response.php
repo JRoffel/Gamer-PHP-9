a3c2561dc850658c8762dc8355899bb15e616b28
LukeKing313 <lucas.assink97@gmail.com>
2015-03-26 13:36:41 -0700

    Version 0.1. response/style
    stuff happened 'n shit

--- response.php
+++ response.php
@@ -8,7 +8,7 @@
 	<div class="container">
 		<h1>Jouw profiel</h1>
 		<p>Je favoriete spel is <?= $_POST['game'] ?></p>
-
+		<p>Je favoriete genre is <?= $_POST['genre'] ?></p>
 	</div>
 </body>
 </html>
