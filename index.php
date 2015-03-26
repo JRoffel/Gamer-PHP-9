d1c73889ae5c8395cbc1410378e99b9e77f03c04
JRoffel <jasonroffel@hotmail.nl>
2015-03-26 13:13:34 +0100

    Version 0.0.2
    :panda_face:

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
 </head>
 <body>
 	<div class="container">
-	
+		<form method="post" action="response.php">
+			<p>
+				<label>Wat is je favoriete spel?</label>
+				<span><input type="text" name="game"/></span>
+			</p>
 
+			<p>
+				<label>&nbsp;</label>
+				<span><input type="submit" value="Send"/></span>
+			</p>
+		</form>
 	</div>
 </body>
 </html>
