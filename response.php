d9405030ea6e3cecc62287df2ef2044d248e949d
LukeKing313 <lucas.assink97@gmail.com>
2015-03-26 14:00:36 -0700

    Version 0.2 bugfiexus
    fetus

--- response.php
+++ response.php
@@ -9,23 +9,20 @@
 		<h1>Jouw profiel</h1>
 		<p>Je favoriete spel is <?= $_POST['game'] ?></p>
 		<p>Je favoriete genre is <?= $_POST['genre'] ?></p>
-		<p>Je speelt 
-			<?=
+			<?php
 			  if ($_POST['count'] == 'one')
 			  {
-			    echo '<p>minder dan 5</p>';
+			    echo '<p>Je speelt minder dan 5 spellen per jaar</p>';
 			  }
 			  elseif ($_POST['count'] == 'two')
 			  {
-			    echo '<p>tussen de 5 en de 15</p>';
+			    echo '<p>Je speelt tussen de 5 en de 15 spellen per jaar</p>';
 			  }
 			  elseif ($_POST['count'] == 'three')
 			  {
-			    echo '<p>meer dan 15</p>';
+			    echo '<p>Je speelt meer dan 15 spellen per jaar</p>';
 			  }
-			?> 
-		per jaar</p>
-</p>
+			?>
 	</div>
 </body>
 </html>
