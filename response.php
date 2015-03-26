8791843b7f189f51e6b342bee0eed5cdb4123272
LukeKing313 <lucas.assink97@gmail.com>
2015-03-26 13:54:38 -0700

    Version 0.2.response

--- response.php
+++ response.php
@@ -9,6 +9,23 @@
 		<h1>Jouw profiel</h1>
 		<p>Je favoriete spel is <?= $_POST['game'] ?></p>
 		<p>Je favoriete genre is <?= $_POST['genre'] ?></p>
+		<p>Je speelt 
+			<?=
+			  if ($_POST['count'] == 'one')
+			  {
+			    echo '<p>minder dan 5</p>';
+			  }
+			  elseif ($_POST['count'] == 'two')
+			  {
+			    echo '<p>tussen de 5 en de 15</p>';
+			  }
+			  elseif ($_POST['count'] == 'three')
+			  {
+			    echo '<p>meer dan 15</p>';
+			  }
+			?> 
+		per jaar</p>
+</p>
 	</div>
 </body>
 </html>
