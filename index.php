b354d98de9eaa269588ddf19b2c800493c103650
LukeKing313 <lucas.assink97@gmail.com>
2015-03-26 13:04:00 -0700

    Version 0.0.1
    the beginning towards a new end

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Gamer</title>
 </head>
 <body>
+	<div class="container">
+	
 
+	</div>
 </body>
 </html>
